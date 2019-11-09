73445371e2014de7498ec71f06f6cea74987f32f
nasibudesign <nasibu.hussein@yahoo.com>
2019-11-10 02:23:15 +0300

    gallery update

--- js/main.js
+++ js/main.js
@@ -14,11 +14,13 @@ $(document).ready(function(){
         return false;
     })
 
-    $('.project-area .button-group #btn1').trigger('click');
+    
 
     $('.project-area .grid .test-popup-link').magnificPopup({
         type: 'image',
         gallery:{enabled:true}
       });
+    
+    $('.project-area .button-group #btn1').trigger('click');
 
 });
