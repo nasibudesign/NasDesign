a0949c379453c2e19757198a8f76be0433d1ea9a
nasibudesign <nasibu.hussein@yahoo.com>
2019-11-10 03:07:37 +0300

    testimonial section

--- js/main.js
+++ js/main.js
@@ -14,13 +14,27 @@ $(document).ready(function(){
         return false;
     })
 
-    
+    $('.project-area .button-group #btn1').trigger('click');
 
     $('.project-area .grid .test-popup-link').magnificPopup({
         type: 'image',
         gallery:{enabled:true}
       });
     
-    $('.project-area .button-group #btn1').trigger('click');
+   //Owl-carousel
+
+   $('.site-main .about-area .owl-carousel').owlCarousel({
+       loop:true,
+       autoplay:true,
+       dots:true,
+       responsive:{
+           0:{
+               items:1
+           },
+           544:{
+               items:2
+           }
+       }
+   })
 
 });
